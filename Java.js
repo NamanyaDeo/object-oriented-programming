de9a49eda1e83fc2cf526aef37b3704e54eb7b06
Golden D <namanyadeo838@gmail.com>
2024-09-07 04:32:44 -0700

    object orientation

---
+++ Java.js
@@ -0,0 +1,16 @@
+/*//These are types of perimitive values in javascript
+let firstName="Deo";//string or string literal
+let age="27";//number or number literal
+let isApproved=true//boolemn or boolemn literal, work with a true or false
+let lastName=undefined//undefined
+let color=null//null*/
+
+//These are reference types in javascript.These include objects,arrays and functions
+//Object-
+let person={
+    name:"Deo",
+    age:"27",
+    color:"green",
+    size:"Small",
+};
+console.log(person);
---
+++ goo.js
@@ -0,0 +1,7 @@
+let person={
+    firstName:"Joa",
+    lastName:"k",
+    location:"Masha",
+    Age:"24",
+};
+console.log(person);
